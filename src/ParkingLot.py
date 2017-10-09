0af31dd7421e5b8eb82f1920d3ff4395e10ce4be
Amit Kumar <amkumar@fb.com>
2017-10-09 10:46:33 +0530

    Parking lot main file

--- src/ParkingLot.py
+++ src/ParkingLot.py
@@ -8,6 +8,7 @@
 
 import heapq
 from Vehicle import Car
+from sys import argv
 
 
 class ParkingLot:
@@ -75,6 +76,61 @@ class ParkingLot:
         else:
             print("Slot does not exist")
 
+    def status(self):
+        try:
+            total_slots_occupied = len(self.slots_occupied["reg_numbers"])
+            if total_slots_occupied>0:
+                print("Slot No.\tRegistration No\tColour")
+                for slot_number in xrange(total_slots_occupied+1):
+                    if slot_number in self.slots_occupied["slots"]:
+                        print("{}\t{}\t{}".format(
+                            self.slots_occupied["slots"][slot_number].parking_slot_id,
+                            self.slots_occupied["slots"][slot_number].parked_car.get_registration(),
+                            self.slots_occupied["slots"][slot_number].parked_car.get_color()
+                        ))
+            else:
+                print("No cars parked yet!")
+        except Exception as e:
+            print("Error in showing parked cards: {}".format(e.message))
+
+    def registration_numbers_for_cars_with_colour(self, colour):
+        try:
+            cars = self.slots_occupied["colors"][colour]
+            if len(cars) == 0:
+                print("No Cars parked with this color!")
+                return
+            reg_of_parked_cars = ""
+            for car in cars:
+                reg_of_parked_cars += car.parked_car.get_registration()+", "
+            reg_of_parked_cars = reg_of_parked_cars[:-2]
+            print(reg_of_parked_cars)
+        except KeyError:
+            print("No Cars parked!")
+
+    def slot_numbers_for_cars_with_colour(self, colour):
+        try:
+            cars = self.slots_occupied["colors"][colour]
+            if len(cars) == 0:
+                print("No Cars parked with this color!")
+                return
+            slot_of_parked_cars = ""
+            for car in cars:
+                slot_of_parked_cars += str(car.parking_slot_id)+", "
+            slot_of_parked_cars = slot_of_parked_cars[:-2]
+            print(slot_of_parked_cars)
+        except KeyError:
+            print("No Cars parked!")
+
+    def slot_number_for_registration_number(self, reg_number):
+        try:
+            car = self.slots_occupied["reg_numbers"][reg_number]
+            if not car:
+                print("Not found")
+                return
+            print(car.parking_slot_id)
+        except KeyError:
+            print("Not found")
+
 
 class ParkingSlot:
     def __init__(self, slot_id = None):
@@ -115,3 +171,8 @@ if __name__ == '__main__':
     p.park_vehicle('KA-01- HH-1534', 'Red')
     p.park_vehicle('KA-01- HH-1634', 'Grey')
     p.park_vehicle('KA-01- HH-1734', 'White')
+    p.status()
+    p.registration_numbers_for_cars_with_colour("White")
+    p.slot_numbers_for_cars_with_colour("White")
+    p.slot_number_for_registration_number("KA-01- HH-1734")
+    p.slot_number_for_registration_number("KA-01- HH-1732")
